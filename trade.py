fe38e2b5a80b14c84bfbb884265e2e0d94a39515
WilliamMarchek <39922188+wilbobraggins@users.noreply.github.com>
2020-09-11 14:44:39 -0400

    python setup for alpaca

---
+++ trade.py
@@ -0,0 +1,41 @@
+import requests, json
+from config import *
+
+BASE_URL = "https://paper-api.alpaca.markets"
+ACCOUNT_URL = "{}/v2/account".format(BASE_URL)
+HEADERS = {"APCA-API-KEY-ID":API_KEY, "APCA-API-SECRET-KEY":SECRET_KEY}
+ORDERS_URL = "{}/v2/orders".format(BASE_URL)
+POSITIONS_URL = "{}/v2/positions".format(BASE_URL)
+
+def get_account():
+    r  = requests.get(ACCOUNT_URL, headers=HEADERS)
+    return json.loads(r.content)
+
+def create_order(symbol, qty, side, type, time_in_force):
+    data = {
+        "symbol": symbol,
+        "qty": qty,
+        "side": side,
+        "type": type,
+        "time_in_force": time_in_force
+    }
+    r = requests.post(ORDERS_URL, json=data, headers=HEADERS)
+    return json.loads(r.content)
+
+def get_orders():
+    r = requests.get(ORDERS_URL, headers=HEADERS)
+
+    return json.loads(r.content)
+    
+def get_position(sym):
+    r = requests.get(POSITIONS_URL, {"sym":sym}, headers=HEADERS)
+
+    return json.loads(r.content)
+
+#response = create_order("AAPL", 100, "buy", "market", "gtc")
+
+#orders = get_orders()
+#account = get_account()
+pos = get_position("AAPL")
+print(pos)
+
